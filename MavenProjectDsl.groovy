c0ce36f22bd415475f6e0b181c3856153d00ae24
sauravk269 <sauravkumar265@gmail.com>
2023-09-20 15:48:12 +0530

    dsl

---
+++ MavenProjectDsl.groovy
@@ -0,0 +1,15 @@
+job('First maven project via dsl'){
+    description("first maven job jenerated on ${new Date()}")
+    scm{
+        git("https://github.com/sauravk269/LatestApiTest.git",master)
+    }
+    triggers{
+        scm('*****')
+    }
+    steps{
+        maven('clean package')
+    }
+    publishers{
+        archiveArtifacts'**/*.jar'
+    }
+}
